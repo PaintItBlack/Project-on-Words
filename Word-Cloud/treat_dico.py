c2f200a1d5c5b20eebfbf218e50174ebbbf47fee
eacquarone <aenguerand@yahoo.fr>
2015-03-16 19:58:49 +0100

    Update treat_dico.py

--- Word-Cloud/treat_dico.py
+++ Word-Cloud/treat_dico.py
@@ -1,9 +1,11 @@
+#We fill our dictionary with the words' frequency of occurence in the text.
+
 def remplir_dico(dico,liste):
     for c in liste :
         dico[c] = dico.get(c,0) + 1
     return(dico)
     
-    
+# There, we only keep words whose frequencies are more than 3 occurences    
     
 def nettoie_dico(dico):
     h = {}
@@ -13,7 +15,7 @@ def nettoie_dico(dico):
     return(h)
     
     
-#fonction donnant la fréquence maximale d'apparition d'un mot dans le dictionnaire
+#function givng the highest number of occurences of a word in a text
 def max (dico) :
 	max=0
 	for mot in dico :
