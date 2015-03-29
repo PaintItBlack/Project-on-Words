1bdbf7e0c9b483d8ace30dc0d000f5b8b7ffd3a1
Fafuh <lamcinel@yahoo.fr>
2015-03-29 17:11:47 +0200

    Update make_articles_dico.py

--- Scraping/make_articles_dico.py
+++ Scraping/make_articles_dico.py
@@ -12,11 +12,10 @@ def make_articles_dico (paper_name, papers_dico) :
     for article in paper.articles :
         title = article.title
         if title != "404 Page Not Found" : #If title not found, just forget it
-            for character in title :
-                title=title.replace("  ", "")
-                title=title.replace("\n", "")
-                title=title.replace("\t", "")
-                title=title.replace("\r", "")
+            title=title.replace("  ", "")
+            title=title.replace("\n", "")
+            title=title.replace("\t", "")
+            title=title.replace("\r", "")
             if title not in ["", " "] : #If title == bullshit, just forget it   
                 if len(title) > 10: #You don't want a lengthy name for your file
                     title=title[:10]
