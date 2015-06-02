4f74aeee86814fb487e49808958156a378465da7
Fafuh <lamcinel@yahoo.fr>
2015-06-02 10:44:35 +0200

    Update oldest_files_in_tree.py

--- Scraping/oldest_files_in_tree.py
+++ Scraping/oldest_files_in_tree.py
@@ -1,14 +1,5 @@
-
-def dist (l_words, l_sen): #Another way would be to have 
-    length = len(l_words)
-    d_mat = zeros(length,length)
-    for i in range(length):
-        word1=l_words[i]
-        for j in range(i):
-            word2=l_words[j]
-            for sentence in l_sen:
-                test1 = word1 in sentence
-                test2 = word2 in sentence
-                if ((not test1) and test2) or (test1 and (not test2)):
-                    d_mat[i][j] += 1
-    return d_mat
+urlpatterns = patterns('Tree_Show.views',
+    url(r'^$', 'Tree_Appear'),
+    url(r'^(?P<name>[-\w\d]+)$', 'Paper_Appear'), #/paper
+    url(r'^(?P<paper>[-\w\d]+)/(?P<slug>[-\w\d]+),(?P<pk>\d+)/$', 'Article_Appear') #/paper/slug,id/
+                       )
