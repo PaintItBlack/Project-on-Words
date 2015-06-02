32bd0ce6f958b97b5844a0872c5e68357c9fd299
Fafuh <lamcinel@yahoo.fr>
2015-06-02 10:56:39 +0200

    Update oldest_files_in_tree.py

--- Scraping/oldest_files_in_tree.py
+++ Scraping/oldest_files_in_tree.py
@@ -1,6 +1,4 @@
 #lol
-urlpatterns = patterns('Tree_Show.views',
-    url(r'^$', 'Tree_Appear'),
-    url(r'^(?P<name>[-\w\d]+)$', 'Paper_Appear'), #/paper
-    url(r'^(?P<paper>[-\w\d]+)/(?P<slug>[-\w\d]+),(?P<pk>\d+)/$', 'Article_Appear') #/paper/slug,id/
-                       )
+def Article_Appear(request, paper, slug, pk): #lowest level tree
+        article = get_object_or_404(Article_Base, pk = pk) #get the article thanks to the unique id=pk from the url request
+        return render(request, "Article_Show.html", {"dicojson" : article.dico, "url" : article.url}) #Calls the template Article_Show.html ; gives it a dictionary and a url
